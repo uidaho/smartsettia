2c6fd327b12aa5d0b4c8eab6a7208bfa6301dcf0
Brandon Jank <jank6275@vandals.uidaho.edu>
2017-08-13 15:55:07 -0700

    create home controller functions and remove auth requirement.

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -13,16 +13,36 @@ class HomeController extends Controller
      */
     public function __construct()
     {
-        $this->middleware('auth');
+        //$this->middleware('auth');
     }
 
     /**
-     * Show the application dashboard.
+     * Show the application home.
      *
      * @return \Illuminate\Http\Response
      */
     public function index()
     {
         return view('home');
+    }
+
+    /**
+     * Show the about page.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function about()
+    {
+    	return view('home.about');
+    }
+
+    /**
+     * Show the help page.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function help()
+    {
+    	return view('home.help');
     }
 }
