2d57ee62d870e3cf1150acd3cfa3828f4c4792d5
Brandon Jank <jank6275@vandals.uidaho.edu>
2017-08-13 16:28:41 -0700

    fix routes

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -15,7 +15,6 @@
 
                     <div class="col-md-6">
                         <input id="email" type="email" class="form-control" name="email" value="{{ old('email') }}" required autofocus>
-
                         @if ($errors->has('email'))
                             <span class="help-block">
                                 <strong>{{ $errors->first('email') }}</strong>
@@ -26,10 +25,8 @@
 
                 <div class="form-group{{ $errors->has('password') ? ' has-error' : '' }}">
                     <label for="password" class="col-md-4 control-label">Password</label>
-
                     <div class="col-md-6">
                         <input id="password" type="password" class="form-control" name="password" required>
-
                         @if ($errors->has('password'))
                             <span class="help-block">
                                 <strong>{{ $errors->first('password') }}</strong>
@@ -50,13 +47,8 @@
 
                 <div class="form-group">
                     <div class="col-md-8 col-md-offset-4">
-                        <button type="submit" class="btn btn-primary">
-                            Login
-                        </button>
-
-                        <a class="btn btn-link" href="{{ route('password.request') }}">
-                            Forgot Your Password?
-                        </a>
+                        <button type="submit" class="btn btn-primary">Login</button>
+                        <a class="btn btn-link" href="{{ route('password.request') }}">Forgot Your Password?</a>
                     </div>
                 </div>
             </form>
--- resources/views/auth/passwords/reset.blade.php
+++ resources/views/auth/passwords/reset.blade.php
@@ -1,4 +1,4 @@
-@extends('layouts.app')
+@extends('layouts.home')
 
 @section('title', 'Reset Password')
 
