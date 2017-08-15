a888b55b07654043afbb9dcd5bbeead4f27a968d
Brandon Jank <jank6275@vandals.uidaho.edu>
2017-08-14 20:29:44 -0700

    semicolon

--- app/Helpers/Helper.php
+++ app/Helpers/Helper.php
@@ -7,6 +7,6 @@ use Illuminate\Html\HtmlBuilder;
 class Helper {
     public static function linkActive(string $name, string $title = null, array $parameters = array(), array $attributes = array())
     {
-        return '<li class="' . Route::currentRouteNamed($name) ? 'active' : '' . '" role="presentation">' . linkRoute($name, $title, $parameters, $attributes) .'</li>'
+        return '<li class="' . Route::currentRouteNamed($name) ? 'active' : '' . '" role="presentation">' . linkRoute($name, $title, $parameters, $attributes) .'</li>';
     }
 }
