e16ca74d34ca969d3eb8e9f3cfbcaee204014e3f
Brandon Jank <jank6275@vandals.uidaho.edu>
2017-08-18 21:40:46 -0700

    fix login page formatting

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -6,49 +6,48 @@
 	<div class="register-photo">
 		<div class="form-container">
 			<div class="image-holder"></div>
-			<form method="POST" action="{{ route('login') }}">
+			<form method="POST" action="{{ route('login') }}" class="form-horizontal">
                 {{ csrf_field() }}
-				<h2 class="text-center">Log in</h2>
+				<h2>Log in</h2>
 
                 <div class="form-group{{ $errors->has('email') ? ' has-error' : '' }}">
-                    <label for="email" class="col-md-4 control-label">E-Mail Address</label>
-
-                    <div class="col-md-6">
-                        <input id="email" type="email" class="form-control" name="email" value="{{ old('email') }}" required autofocus>
-                        @if ($errors->has('email'))
-                            <span class="help-block">
-                                <strong>{{ $errors->first('email') }}</strong>
-                            </span>
-                        @endif
-                    </div>
+                    <label for="email" class="col-xs-3 control-label">Email</label>
+					<div class="col-xs-9">
+						<input id="email" type="email" class="form-control" name="email" value="{{ old('email') }}" required autofocus>
+	                    @if ($errors->has('email'))
+	                        <span class="help-block">
+	                            <strong>{{ $errors->first('email') }}</strong>
+	                        </span>
+	                    @endif
+					</div>
                 </div>
 
                 <div class="form-group{{ $errors->has('password') ? ' has-error' : '' }}">
-                    <label for="password" class="col-md-4 control-label">Password</label>
-                    <div class="col-md-6">
-                        <input id="password" type="password" class="form-control" name="password" required>
-                        @if ($errors->has('password'))
-                            <span class="help-block">
-                                <strong>{{ $errors->first('password') }}</strong>
-                            </span>
-                        @endif
-                    </div>
+                    <label for="password" class="col-xs-3 control-label">Password</label>
+                    <div class="col-xs-9">
+						<input id="password" type="password" class="form-control" name="password" required>
+	                    @if ($errors->has('password'))
+	                        <span class="help-block">
+	                            <strong>{{ $errors->first('password') }}</strong>
+	                        </span>
+	                    @endif
+					</div>
                 </div>
 
                 <div class="form-group">
-                    <div class="col-md-6 col-md-offset-4">
-                        <div class="checkbox">
-                            <label>
-                                <input type="checkbox" name="remember" {{ old('remember') ? 'checked' : '' }}> Remember Me
-                            </label>
-                        </div>
-                    </div>
+					<div class="col-xs-offset-3 col-xs-9">
+						<div class="checkbox">
+							<label>
+								<input type="checkbox" name="remember" {{ old('remember') ? 'checked' : '' }}> Remember Me
+							</label>
+						</div>
+					</div>
                 </div>
 
                 <div class="form-group">
-                    <div class="col-md-8 col-md-offset-4">
+                    <div class="col-xs-offset-3 col-xs-9">
                         <button type="submit" class="btn btn-primary">Login</button>
-                        <a class="btn btn-link" href="{{ route('password.request') }}">Forgot Your Password?</a>
+                        <p><a class="margin-top-4" href="{{ route('password.request') }}">Forgot Your Password?</a></p>
                     </div>
                 </div>
             </form>
