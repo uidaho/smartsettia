54eaee067013680786782b85fccad2f672a1512a
brec9824 <brec9824@users.noreply.github.com>
2017-08-31 16:26:11 -0700

    Convert tabs to spaces

--- database/migrations/2017_08_31_170510_create_schedules_table.php
+++ database/migrations/2017_08_31_170510_create_schedules_table.php
@@ -6,30 +6,30 @@ use Illuminate\Database\Migrations\Migration;
 
 class CreateSchedulesTable extends Migration
 {
-	/**
-	 * Run the migrations.
-	 *
-	 * @return void
-	 */
-	public function up()
-	{
-		Schema::create('schedules', function (Blueprint $table) {
-			$table->increments('id');
-			$table->integer('type')->unsigned();                        # 0 = open, 1 = close
-			$table->integer('time')->unsigned();                        # Military time example: 7pm -> 1900
-			$table->integer('device_id')->unsigned();
-			
-			$table->foreign('device_id')->references('id')->on('devices')->onDelete('cascade');
-		});
-	}
-	
-	/**
-	 * Reverse the migrations.
-	 *
-	 * @return void
-	 */
-	public function down()
-	{
-		Schema::dropIfExists('schedules');
-	}
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('schedules', function (Blueprint $table) {
+            $table->increments('id');
+            $table->integer('type')->unsigned();                        # 0 = open, 1 = close
+            $table->integer('time')->unsigned();                        # Military time example: 7pm -> 1900
+            $table->integer('device_id')->unsigned();
+            
+            $table->foreign('device_id')->references('id')->on('devices')->onDelete('cascade');
+        });
+    }
+    
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('schedules');
+    }
 }
--- database/migrations/2017_08_31_170547_create_sensor_data_table.php
+++ database/migrations/2017_08_31_170547_create_sensor_data_table.php
@@ -6,30 +6,30 @@ use Illuminate\Database\Migrations\Migration;
 
 class CreateSensorDataTable extends Migration
 {
-	/**
-	 * Run the migrations.
-	 *
-	 * @return void
-	 */
-	public function up()
-	{
-		Schema::create('sensor_data', function (Blueprint $table) {
-			$table->increments('id');
-			$table->integer('sensor_id')->unsigned();
-			$table->string('value');
-			$table->timestamp('created_at')->useCurrent();
-			
-			$table->foreign('sensor_id')->references('id')->on('sensors')->onDelete('cascade');
-		});
-	}
-	
-	/**
-	 * Reverse the migrations.
-	 *
-	 * @return void
-	 */
-	public function down()
-	{
-		Schema::dropIfExists('sensor_data');
-	}
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('sensor_data', function (Blueprint $table) {
+            $table->increments('id');
+            $table->integer('sensor_id')->unsigned();
+            $table->string('value');
+            $table->timestamp('created_at')->useCurrent();
+            
+            $table->foreign('sensor_id')->references('id')->on('sensors')->onDelete('cascade');
+        });
+    }
+    
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('sensor_data');
+    }
 }
