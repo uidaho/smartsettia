f0bc3d9d54637e5d4393771e033a16c6bb50dc2d
Brandon Jank <jank6275@vandals.uidaho.edu>
2017-09-10 02:30:49 -0700

    refactor user controller

---
+++ resources/views/user/create.blade.php
@@ -0,0 +1,75 @@
+@extends('layouts.dashboard')
+
+@section('title', 'Create User')
+
+@section('content')
+<div class="container">
+    <div class="row">
+        <div class="col-md-8 col-md-offset-2">
+            <div class="panel panel-default">
+                <div class="panel-heading">Create User</div>
+                <div class="panel-body">
+                    {!! Form::open(['route' => 'user.store', 'method' => 'post', 'class' => 'form-horizontal']) !!}
+                        {{ csrf_field() }}
+
+                        <div class="form-group{{ $errors->has('name') ? ' has-error' : '' }}">
+                            {!! Form::label('name', 'Name', ['class' => 'col-sm-3 control-label']) !!}
+                            <div class="col-sm-9">
+                                {!! Form::text('name', null, ['class' => 'form-control', 'required' => 'required']) !!}
+                                <small class="text-danger">{{ $errors->first('name') }}</small>
+                            </div>
+                        </div>
+
+                        <div class="form-group{{ $errors->has('email') ? ' has-error' : '' }}">
+                            {!! Form::label('email', 'Email address', ['class' =>'col-sm-3 control-label']) !!}
+                            <div class="col-sm-9">
+                                {!! Form::email('email', null, ['class' => 'form-control', 'required' => 'required', 'placeholder' => 'eg: foo@bar.com']) !!}
+                                <small class="text-danger">{{ $errors->first('email') }}</small>
+                            </div>
+                        </div>
+
+                        <div class="form-group{{ $errors->has('password') ? ' has-error' : '' }}">
+                            {!! Form::label('password', 'Password', ['class' => 'col-sm-3 control-label']) !!}
+                                <div class="col-sm-9">
+                                    {!! Form::password('password', ['class' => 'form-control', 'required' => 'required']) !!}
+                                    <small class="text-danger">{{ $errors->first('password') }}</small>
+                                </div>
+                        </div>
+
+                        <div class="form-group{{ $errors->has('password-confirm') ? ' has-error' : '' }}">
+                            {!! Form::label('password-confirm', 'Confirm Password', ['class' => 'col-sm-3 control-label']) !!}
+                                <div class="col-sm-9">
+                                    {!! Form::password('password-confirm', ['class' => 'form-control', 'name' => 'password_confirmation', 'required' => 'required']) !!}
+                                    <small class="text-danger">{{ $errors->first('password-confirm') }}</small>
+                                </div>
+                        </div>
+
+                        <div class="form-group{{ $errors->has('role') ? ' has-error' : '' }}">
+                            {!! Form::label('role', 'User Role', ['class' => 'col-sm-3 control-label']) !!}
+                                <div class="col-sm-9">
+                                    {!! Form::select('role', [0 => 'Registered - No access', 1 => 'User - Can control units', 2 => 'Manager - Can modify units and users', 3 => 'Owner - Can create managers'], null, ['placeholder' => 'Pick a role...', 'class' => 'form-control', 'name' => 'role', 'required' => 'required']) !!}
+                                    <small class="text-danger">{{ $errors->first('role') }}</small>
+                                </div>
+                        </div>
+
+                        <div class="form-group">
+                            <div class="col-md-6 col-md-offset-4">
+                                {!! Form::submit('Create User', ['class' => 'btn btn-info pull-right']) !!}
+                            </div>
+                        </div>
+                    {!! Form::close() !!}
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+@endsection
+
+@push('styles')
+
+@endpush
+
+
+@push('scripts')
+
+@endpush
--- resources/views/user/edit.blade.php
+++ resources/views/user/edit.blade.php
@@ -1,15 +1,17 @@
 @extends('layouts.dashboard')
 
-@section('title', 'Users')
+@section('title', 'Edit User')
 
 @section('content')
 <div class="container">
     <div class="row">
         <div class="col-md-8 col-md-offset-2">
             <div class="panel panel-default">
-                <div class="panel-heading">Create User</div>
+                <div class="panel-heading">Edit User</div>
                 <div class="panel-body">
-                    {!! Form::open(['route' => 'users.store', 'method' => 'post', 'class' => 'form-horizontal']) !!}
+                    {!! Form::model($user, ['route' => ['user.update', $user->id], 'method' => 'post', 'class' => 'form-horizontal']) !!}
+                        {{ method_field('PUT') }}
+
                         {{ csrf_field() }}
 
                         <div class="form-group{{ $errors->has('name') ? ' has-error' : '' }}">
@@ -28,20 +30,12 @@
                             </div>
                         </div>
 
-                        <div class="form-group{{ $errors->has('password') ? ' has-error' : '' }}">
-                            {!! Form::label('password', 'Password', ['class' => 'col-sm-3 control-label']) !!}
-                                <div class="col-sm-9">
-                                    {!! Form::password('password', ['class' => 'form-control', 'required' => 'required']) !!}
-                                    <small class="text-danger">{{ $errors->first('password') }}</small>
-                                </div>
-                        </div>
-
-                        <div class="form-group{{ $errors->has('password-confirm') ? ' has-error' : '' }}">
-                            {!! Form::label('password-confirm', 'Confirm Password', ['class' => 'col-sm-3 control-label']) !!}
-                                <div class="col-sm-9">
-                                    {!! Form::password('password-confirm', ['class' => 'form-control', 'name' => 'password_confirmation', 'required' => 'required']) !!}
-                                    <small class="text-danger">{{ $errors->first('password-confirm') }}</small>
-                                </div>
+                        <div class="form-group{{ $errors->has('phone') ? ' has-error' : '' }}">
+                            {!! Form::label('phone', 'Phone number', ['class' => 'col-sm-3 control-label']) !!}
+                            <div class="col-sm-9">
+                                {!! Form::text('phone', null, ['class' => 'form-control']) !!}
+                                <small class="text-danger">{{ $errors->first('phone') }}</small>
+                            </div>
                         </div>
 
                         <div class="form-group{{ $errors->has('role') ? ' has-error' : '' }}">
@@ -54,7 +48,7 @@
 
                         <div class="form-group">
                             <div class="col-md-6 col-md-offset-4">
-                                {!! Form::submit('Create User', ['class' => 'btn btn-info pull-right']) !!}
+                                {!! Form::submit('Save', ['class' => 'btn btn-info pull-right']) !!}
                             </div>
                         </div>
                     {!! Form::close() !!}
