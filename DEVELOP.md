eeb5355b1a843762b1403b180f524f3f2ef218ec
Brandon Jank <brandon@jank.io>
2017-09-11 21:28:20 +0000

    enable markdown support

--- DEVELOP.md
+++ DEVELOP.md
@@ -37,8 +37,8 @@ sudo apt upgrade
 ### Install PHP7, NGINX, and redis-server
 ```bash
 sudo apt install curl unzip git
-sudo apt install php7.0 php7.0-mbstring php7.0-gd php7.0-xml php7.0-cli php7.0-mysql composer
-sudo apt install redis-server nginx
+sudo apt install php7.0 php7.0-fpm php7.0-curl php7.0-dev php7.0-gd php7.0-intl php7.0-mcrypt php7.0-json php7.0-mysql php7.0-opcache php7.0-bcmath php7.0-mbstring php7.0-soap php7.0-xml php7.0-cli
+sudo apt install composer redis-server nginx
 ```
 
 ### Install MySQL
--- config/app.php
+++ config/app.php
@@ -185,6 +185,8 @@ return [
         Yajra\Datatables\ButtonsServiceProvider::class,
         
         Maatwebsite\Excel\ExcelServiceProvider::class,
+        
+        GrahamCampbell\Markdown\MarkdownServiceProvider::class,
 
     ],
 
@@ -223,6 +225,7 @@ return [
         'Lang' => Illuminate\Support\Facades\Lang::class,
         'Log' => Illuminate\Support\Facades\Log::class,
         'Mail' => Illuminate\Support\Facades\Mail::class,
+        'Markdown' => GrahamCampbell\Markdown\Facades\Markdown::class,
         'Notification' => Illuminate\Support\Facades\Notification::class,
         'Password' => Illuminate\Support\Facades\Password::class,
         'Queue' => Illuminate\Support\Facades\Queue::class,
