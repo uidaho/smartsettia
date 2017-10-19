7d32ed6ea6f6b9f38bbe9138ed5838e471883b0a
Brandon Jank <brandon@jank.io>
2017-10-19 01:33:19 -0700

    clean-up user views

--- resources/views/user/action.blade.php
+++ resources/views/user/action.blade.php
@@ -1,8 +1,11 @@
 <div class="btn-group" role="group">
-    <a href="/user/{{ $id }}/edit" class="btn btn-xs btn-primary">
+    <a href="{{ route('user.show', $id) }}" class="btn btn-xs btn-primary">
+        <i class="glyphicon glyphicon-eye-open"></i> Show
+    </a>
+    <a href="{{ route('user.edit', $id) }}" class="btn btn-xs btn-primary">
         <i class="glyphicon glyphicon-edit"></i> Edit
     </a>
-    <a href="/user/{{ $id }}/remove" class="btn btn-xs btn-primary btn-danger">
-        <i class="glyphicon glyphicon-remove"></i> Delete
-    </a>
+    {!! Form::open(['method' => 'DELETE', 'route' => ['user.destroy', $id], 'style' => 'display:inline', 'onsubmit' => 'return confirm("Are you sure you want to delete the user named '.$name.'?")']) !!}
+        {!! Form::button('<i class="glyphicon glyphicon-remove"></i> Delete', ['type' => 'submit', 'class' => 'btn btn-xs btn-danger', 'title' => 'Delete this user']) !!}
+    {!! Form::close() !!}
 </div>
