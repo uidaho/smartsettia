4f6749afca98a4e1577bc57d98376232057cee7f
Nick <kren5141@vandals.uidaho.edu>
2017-12-05 06:04:18 +0000

    Migration to change default rates on update and image

---
+++ database/migrations/2017_12_04_change_default_job_rate_devices_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class ReAddLastNetworkUpdateAtToDevicesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('devices', function (Blueprint $table) {
+            $table->integer('update_rate')->default(4);
+        });
+        Schema::table('devices', function (Blueprint $table) {
+            $table->integer('image_rate')->default(15);
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('devices', function (Blueprint $table) {
+            $table->integer('update_rate')->default(60);
+        });
+        Schema::table('devices', function (Blueprint $table) {
+            $table->integer('image_rate')->default(60);
+        });
+    }
+}
